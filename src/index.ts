9d77ce518af5475a1726b83a60cd8ca1683e0b9d
petrushi <pavlovinthesky@gmail.com>
2021-12-22 04:36:06 +0300

    still requesting

--- src/index.ts
+++ src/index.ts
@@ -102,7 +102,7 @@ async function getBlockInfo(blockID: string) {
         }
       )
       .on("error", (err) => {
-        console.log("\nError from API: " + err.message);
+        console.log("\nError from API: " + err.message + "Keep making requests still.");
       });
     await delay(1000);
   }
